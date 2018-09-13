6c10e64c5db534c2b06f37e35de2e3a8aa3a883f
Ben <ben@bytevoid.net>
2018-09-12 18:41:38 -0700

    Commit empty starter project.

---
+++ CodingExercise.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingExercise.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
